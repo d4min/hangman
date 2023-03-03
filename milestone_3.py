d5ff3ccfca2c2816e56c0b7a8a0160f603c6b05b
d4min <danishamin.rb@gmail.com>
2023-03-03 19:14:33 +0000

    Checks if the user input 'guess' is in the word to be guessed and prints out a statement if it is and if it isn't

--- milestone_3.py
+++ milestone_3.py
@@ -7,3 +7,7 @@ while True:
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
         
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
