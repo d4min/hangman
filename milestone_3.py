f4816851e4e33460418a61ec85a7e25be27c515c
d4min <danishamin.rb@gmail.com>
2023-03-03 19:38:14 +0000

    Updates .gitignore file to ensure __pycache__ files are not committed. Defines functions for asking for an input from the user as well as checking the input against the word being guessed

--- milestone_3.py
+++ milestone_3.py
@@ -1,13 +1,26 @@
-while True:
-    guess = input("Please enter your first guess as a single character")
+import random
 
-    if len(guess) == 1 and guess.isalpha():
-        print("Good guess!")
-        break
+word_list = ["pineapple", "peaches", "mango", "watermelon", "banana"]
+
+word = random.choice(word_list)
+
+def ask_for_input():
+    while True:
+        guess = input("Please enter your first guess as a single character")
+
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    
+    check_guess(guess)
+
+
+def check_guess(guess): 
+    guess = guess.lower()      
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-        
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+ask_for_input()
