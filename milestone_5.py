ee9e13d820dfcda43932f5ee20d467ebef799ccb
d4min <danishamin.rb@gmail.com>
2023-03-05 02:53:57 +0000

    Adds the milestone_5.py file which defines the function play_game which allows the user to play the Hangman game

---
+++ milestone_5.py
@@ -0,0 +1,60 @@
+import random
+
+class Hangman:
+    
+    def __init__(self, word_list, num_lives = 5):
+        self.word = random.choice(word_list)
+        self.word_guessed = ["_" for i in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
+
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Please enter your first guess as a single character")
+
+            if len(guess) != 1 or guess.isalpha() == False:
+                print("Invalid input. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.list_of_guesses.append(guess)
+                self.check_guess(guess)
+                break
+
+
+    def check_guess(self, guess): 
+        guess = guess.lower()      
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+
+            for i in range(len(self.word)):
+                if self.word[i] == guess:
+                    self.word_guessed[i] = guess
+            print(self.word_guessed)
+            self.num_letters -= 1
+        else:
+            print(f"Sorry, {guess} is not in the word.")
+            self.num_lives -= 1
+            print(f"You have {self.num_lives} lives left.")
+
+def play_game(word_list):
+
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+
+    while True:
+        if game.num_lives == 0:
+            print("You lost!")
+            break
+        elif game.num_letters > 0:
+            game.ask_for_input()
+        else:
+            print("Congratulations. You won the game!")
+            break
+
+word_list = ["pineapple", "peaches", "mango", "watermelon", "banana"]
+
+play_game(word_list)
