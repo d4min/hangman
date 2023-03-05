06fd70cd6e7d59cb9d1ce1f1e27503a93dab45f8
d4min <danishamin.rb@gmail.com>
2023-03-05 02:09:48 +0000

    Defines the ask_for_input method which allows user to input their guess and defines the check_guess method to check if the input is in the word to be guessed

--- milestone_4.py
+++ milestone_4.py
@@ -10,3 +10,27 @@ class Hangman:
         self.word_list = word_list
         self.list_of_guesses = []
 
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Please enter your first guess as a single character")
+
+            if len(guess) != 1 or guess.isalpha() == False:
+                print("Invalid input. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.list_of_guesses.append(guess)
+                self.check_guess(guess)
+                break
+                
+    
+        self.check_guess(guess)
+
+    def check_guess(self, guess): 
+        guess = guess.lower()      
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+        else:
+            print(f"Sorry, {guess} is not in the word. Try again.")
+
